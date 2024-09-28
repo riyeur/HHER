bbaccdf1063453c4606fd62afd51d12abd6fba97
Hooriya Khan <riyeur@hotmail.com>
2024-09-28 00:55:53 -0600

    Added more infographics + minor website changes

--- client/src/components/InfographicsMiddle.js
+++ client/src/components/InfographicsMiddle.js
@@ -17,7 +17,12 @@ const InfographicsMiddle = () => {
         { id: 10, src: '../images/infographics/womenshealthweek.png', alt: "Women's Health Week" },
         { id: 11, src: '../images/infographics/mentalhealth.png', alt: 'Mental Health' },
         { id: 12, src: '../images/infographics/pcos.png', alt: 'PCOS' },
-        { id: 13, src: '../images/infographics/researchgaps.png', alt: 'Research Gaps' }
+        { id: 13, src: '../images/infographics/researchgaps.png', alt: 'Research Gaps' },
+        { id: 14, src: '../images/infographics/pcosmythvsfact.png', alt: 'PCOS (Myth vs Fact)' },
+        { id: 15, src: '../images/infographics/ovariancancerpartone.png', alt: 'Ovarian Cancer (Part 1)' },
+        { id: 16, src: '../images/infographics/ovariancancerparttwo.png', alt: 'Ovarian Cancer (Part 2)' },
+        { id: 17, src: '../images/infographics/indigenousfemalehealth.png', alt: 'Indigenous Female Health' },
+        { id: 18, src: '../images/infographics/anemia.png', alt: 'Anemia' },
     ];
 
     const closeModal = () => {
@@ -30,7 +35,6 @@ const InfographicsMiddle = () => {
                 <p>To view our infographics on this website, please use a laptop/desktop.</p>
                 <p>Or, you can visit this <a className="infographics-page-link" href="https://drive.google.com/drive/folders/1Xjx-J0Iynqi64gpIjAuxnZ-zYCeGVINj" target="_blank" rel="noopener noreferrer">link</a> to see all of them.</p>
             </div>
-            {/* Grid container for the image buttons */}
             <div className="image-button-grid">
                 {images.map(image => (
                     <button
@@ -42,8 +46,7 @@ const InfographicsMiddle = () => {
                     </button>
                 ))}
             </div>
-
-            {/* Render with image */}
+            
             {currentImage && (
                 <div className="modal-overlay" onClick={closeModal}>
                     <div className="modal-content" onClick={(e) => e.stopPropagation()}>
