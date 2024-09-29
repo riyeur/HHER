0c89ee8ce41eb6a0ecf3c1532fbdec65062ca1ff
Hooriya Khan <riyeur@hotmail.com>
2024-09-29 00:48:59 -0600

    infographics page fixed

--- client/src/components/InfographicsMain.js
+++ client/src/components/InfographicsMain.js
@@ -4,12 +4,18 @@ import './styles.css';
 const InfographicsMain = () => {
     return (
         <div className="infographics-main">
-            <img className="heart-left" src="../images/flower_left.png" alt="Left heart" width="100" height="auto" />
+            <div className="flowers-top">
+                <img className="side-flower-left-top" src="../images/side_flower_three.png" alt="Side Flower Left Top" width="200" height="auto" />
+                <img className="side-flower-right-top" src="../images/side_flower_two.png" alt="Side Flower Right Top" width="200" height="auto" />
+            </div>
             <div className="infographics-page">
                 <h5 className="infographics-text">Infographics</h5>
                 <p className="infographics-tagline">Browse our infographics to learn about essential topics in women's health.</p>
             </div>
-            <img className="heart-right" src="../images/flower_right.png" alt="Right heart" width="100" height="auto" />
+            <div className="flowers-bottom">
+                <img className="side-flower-left-bottom" src="../images/side_flower_four.png" alt="Side Flower Left Bottom" width="200" height="auto" />
+                <img className="side-flower-right-bottom" src="../images/side_flower_one.png" alt="Side Flower Right Bottom" width="200" height="auto" />
+            </div>
         </div>
     );
 };
